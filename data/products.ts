cbf5750f56e004900e885d8b92a21691243b769b
Thomas Leconte <thomasl.dw20@gmail.com>
2026-04-22 21:13:55 +0200

    test apparition modif en ligne

--- data/products.ts
+++ data/products.ts
@@ -1,4 +1,5 @@
 export const products = [
+  { id: 1, name: "Test", target: 2 },
   { id: 1, name: "Tomates", target: 10 },
   { id: 2, name: "Salade", target: 8 },
   { id: 3, name: "Poulet mariné", target: 12 },
